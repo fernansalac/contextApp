bb31a5a54e696c4e6648f897d3f6abffc173e556
Fernan Salac <salacfernan@gmail.com>
2020-05-05 02:04:18 +0800

    #4 accessing context

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -5,13 +5,13 @@ export const ThemeContext = createContext();
 class ThemeContextProvider extends Component {
     state = {
         isLightTheme: true,
-        light: { syntax: '#555', ui: '#ddd', bg: 'eee' },
+        light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
         dark: { syntax: '#ddd', ui: '#333', bg: '#555' }
     }
     render() { 
         return (
             <ThemeContext.Provider value={{...this.state}}>
-                {this.props.children}
+                { this.props.children }
             </ThemeContext.Provider>
         );
     }
